f057d093cdd0be4a9568cd78c5ddb3b837f2c66b
Christian <logicismmc@gmail.com>
2023-01-09 09:25:11 +0800

    My VSC rebuilt the cache of my CMake, not sure if that'll affect my setup at home, but making a branch otherwise.

--- build/Nare/cmake_install.cmake
+++ build/Nare/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/User/source/repos/Nare/Nare
+# Install script for directory: C:/Users/logic/Downloads/Nare/Nare/Nare
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
